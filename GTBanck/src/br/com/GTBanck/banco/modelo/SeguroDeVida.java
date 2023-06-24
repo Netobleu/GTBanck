a84878489a3f7763c4b27d925b001a84b47fa56d
Ivony Mesquta Neto <136274725+Netobleu@users.noreply.github.com>
2023-06-24 12:13:05 -0300

    Add files via upload

---
+++ GTBanck/src/br/com/GTBanck/banco/modelo/SeguroDeVida.java
@@ -0,0 +1,10 @@
+package br.com.GTBanck.banco.modelo;
+
+public class SeguroDeVida implements Tributavel {
+
+	@Override
+	public double getValorImposto() {
+		return 42;
+	}
+
+}
---
+++ GTBanck/src/br/com/GTBanck/banco/modelo/Tributavel.java
@@ -0,0 +1,6 @@
+package br.com.GTBanck.banco.modelo;
+
+public interface Tributavel {
+
+	double getValorImposto();
+}
